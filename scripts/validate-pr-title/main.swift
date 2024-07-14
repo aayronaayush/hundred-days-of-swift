e90d0ef740a9dd67524b0031a1d0ac4f948f699b
aayronaayush <55760570+aayronaayush@users.noreply.github.com>
2024-07-14 14:35:17 +0400

    Day 2 | PR Title Validation Pipeline (#1)

---
+++ scripts/validate-pr-title/main.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+enum ValidationError: Error {
+    case invalidTitle
+}
+
+func validateTitle(_ title: String) -> Bool {
+    let pattern = "^Day \\d+ \\| .+$"
+    let regex = try! NSRegularExpression(pattern: pattern)
+    let range = NSRange(location: 0, length: title.utf16.count)
+    return regex.firstMatch(in: title, options: [], range: range) != nil
+}
+
+func main() {
+    let title = CommandLine.arguments[1]
+
+    print("Validating title: [\(title)]")    
+
+    if validateTitle(title) {
+        print("PR Title is valid! :)")
+    } else {
+        print("PR Title Doesn't match the pattern :(")
+        print("Please follow the pattern: Day <day_number> | <description>")
+        exit(1)
+    }
+}
+
+main()
