28f1656dfe310b54b89e338c8c4edf0a5c6dcf2d
Aayush Aayron Deo <aayushaayron@gmail.com>
2024-07-17 23:53:55 +0400

    completed challenge

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Angela Yu on 14/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
+///Users/aayush/Documents/hundred-days-of-swift/Magic-8-Ball-iOS13/Magic 8 Ball/Base.lproj/Main.storyboard
 
 import UIKit
 
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var magic8Ball: UIImageView!
+    
+    @IBAction func onClickAsk() {
+        magic8Ball.image = ballArray.randomElement()
+    }
+    
 }
 
